f2644d3d53ee92ba1ce0b9a8509c742abb3fdb95
danielosnes <osnesdaniel@gmail.com>
2025-08-21 19:02:43 -0700

    add many-to-many start to relationships

---
+++ DatabaseRelationships/5-many-to-manyRelationshipsPt2.sql
@@ -0,0 +1,9 @@
+/*
+Now that you have related two tables with a many-to-many relationship via a cross-reference table,
+you will get to populate the cross-reference table and make interesting queries.
+
+In this exercise, you are going to demonstrate the many-to-many relationship between [book] and [author] through the 
+cross-reference table, [books_authors].
+
+
+*/
