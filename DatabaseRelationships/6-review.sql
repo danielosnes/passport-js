d4b6f27afc62d8e203e49246c45d06d7d63cc805
danielosnes <osnesdaniel@gmail.com>
2025-08-22 10:49:24 -0700

    add review to database relationships

---
+++ DatabaseRelationships/6-review.sql
@@ -0,0 +1,26 @@
+/*
+What Are Database Relationships?
+Review
+<1 min
+Well done! In this lesson, you have accomplished the following:
+
+You learned about one-to-one relationship, created a new table, book_details to show its one-to-one correspondence with book and vice-versa, 
+and imported dummy data and ran queries to validate the keys in the table.
+
+You learned about one-to-many relationship, created a new table, page to show a one-to-many relationship between chapter and page, 
+imported sample data and ran queries to validate the relationship.
+
+You learned about many-to-many relationship, created a cross-reference table, books_authors, to bind both book and author into a many-to-many relationship, 
+populated the table and ran queries to validate the relationship.
+
+At this point, you have designed a solid book database schema that you can be proud of. If you were to import your schema into a database design software, 
+you might have a schema diagram like the one on the right. Does it meet the requirements of a well-defined schema?
+
+Are you able to do the following for your schema?
+
+Name the tables in your schema.
+Name the column names and types for each table in the schema.
+Name the constraints for each table.
+Name the relationships between tables.
+Congratulations for a job well-done!!
+*/
