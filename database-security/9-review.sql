569dd599925cee095f27824814487c8a702db60e
danielosnes <osnesdaniel@gmail.com>
2025-08-27 15:23:49 -0700

    add review to database security

---
+++ database-security/9-review.sql
@@ -0,0 +1,18 @@
+/*
+Wrap-up
+<1 min
+Congratulations, this concludes the lesson on database security with roles. In this section, we’ve covered quite a few concepts related to database security.
+
+Superuser permissions — You learned what a superuser was, what privileges they have, and when to use a superuser role.
+
+Group and login roles — You learned about how login and group roles can be used to manage sets of users.
+
+PostgreSQL internal permissions tables — You learned about the tables you can reference to monitor the privileges of the users in your database. 
+With this knowledge, you’ll be able to safely delegate the correct permissions to other users in the database.
+
+Schema and table level security — You learned about schema and table level security and how enabling default privileges can simplify the permission granting process.
+
+Row and column level security — You learned about using these tools to have finer control over which data within a table a user can access.
+
+Hopefully, this has been helpful in introducing some of the core concepts of PostgreSQL security.
+*/
